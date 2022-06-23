d6899f6c6b9362095a17d442298b4c69c70713c0
Monique <monique.meas@gmail.com>
2022-06-23 16:38:39 -0400

    add interger input for prompt in shop() function

--- assets/js/game.js
+++ assets/js/game.js
@@ -158,24 +158,20 @@ else {
 var shop = function(){
 //ask player what they'd like to do
 var shopOptionPrompt = window.prompt(
-  "Would you like to REFLL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', ot 'LEAVE' to make a choice."
+  "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE to make a choice."
 );
-if (shopOptionPrompt === "refill" || shopOptionPrompt === "REFILL") {
 
-}
 // use switch to carry out action
+shopOptionPrompt = parseInt(shopOptionPrompt);
 switch (shopOptionPrompt){
-  case "REFILL": // new case
-  case "refill":
+  case 1:
     playerInfo.refillHealth();
     break;
 
-  case "UPGRADE":
-  case "upgrade":
+  case 2:
     playerInfo.upgradeAttack();
   break;
-  case "LEAVE":
-  case "leave":
+  case 3:
     window.alert("Leaving the store.");
   //call shop() again to force player to pick a valid option
       break;
