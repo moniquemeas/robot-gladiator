725acec23cfb85488ca085fc89108d2f475217ba
Monique <monique.meas@gmail.com>
2022-06-24 18:59:39 -0400

    save high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -167,22 +167,22 @@ var startGame = function() {
 // function to end the entire game
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
+// check local storage of high score, if it's not there, use 0
+var highScore = localStorage.getItem("highscore");
+if(highScore === null) {
+  highScore = 0;
+}
+//if player has more money that the high score, player has new high score!
+if (playerInfo.money > highScore) {
+  localStorage.setItem("highscore", playerInfo.money);
+  localStorage.setItem("name", playerInfo.name);
+  alert(playerInfo.name + " now has the high score of " + playerInfo.monay + "!");
+}
+else {
+  alert(playerInfo.name + " did not beat the score of " + highScore + ". Maybe next time!");
 
-  // check localStorage for high score, if it's not there, use 0
-  var highScore = localStorage.getItem("highscore");
-  if (highScore === null) {
-    highScore = 0;
-  }
+}
 
-  // if player has more money than the high score, player has new high score!
-  if (playerInfo.money > highScore) {
-    localStorage.setItem("highscore", playerInfo.money);
-    localStorage.setItem("name", playerInfo.name);
-
-    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
-  } else {
-    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
-  }
 
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
